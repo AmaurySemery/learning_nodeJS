659cfdd66211275294f4b782e357a2036a81ad44
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2023-12-15 15:56:20 +0100

    Test synchronous

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 const synchronous = require('./synchronicity/synchronous');
 
-const result = synchronous.getRandomNumberSync(10);
-console.log(`Le nombre généré est : ${result}`);
+console.log('avant for')
+
+for(let i = 0 ; i < 10000 ; i++) {
+
+    console.log('Avant getRandomNumberSync()');
+    const result = synchronous.getRandomNumberSync(10);
+    console.log(`Le nombre généré est : ${result}`);    
+    console.log('Après getRandomNumberSync()');
+    
+}
+
+console.log('après for')
