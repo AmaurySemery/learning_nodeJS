31cac2f79274da740ae69f8eb39da05f556682c6
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2023-12-26 14:17:54 +0100

    Writable stream

--- streams/index.js
+++ streams/index.js
@@ -3,15 +3,30 @@
 
 // readable stream (fichiers déplaçables morceaux par morceaux => économie de mémoire entre autre)
 
-const createReadStream = require('fs').createReadStream;
+//const createReadStream = require('fs').createReadStream;
 
-const readable = createReadStream('./fruits.txt');
+//const readable = createReadStream('./fruits.txt');
 
-readable.on('data', (chunk) => {
-    console.log(chunk.toString());
-    console.log(chunk.length);
+//readable.on('data', (chunk) => {
+//    console.log(chunk.toString());
+//     console.log(chunk.length);
+// });
+
+// readable.on('end', () => {
+//     console.log('fini de lire');
+// });
+
+// writable stream (le verser quelque part)
+
+const { createWriteStream, createReadStream } = require('fs');
+
+const readStream = createReadStream('./fruits.txt');
+const writeStream = createWriteStream('./fruits_copy.txt');
+
+readStream.on('data', (chunk) => {
+    writeStream.write(chunk);
 });
 
-readable.on('end', () => {
-    console.log('fini de lire');
+readStream.on('end', () => {
+    writeStream.end();
 });
